3b5d2b04965800b90243b359886617cc69c3156d
Juhyun Lim <wow5468@naver.com>
2017-09-11 16:47:35 +0900

    [WHATSIT-115] Modified some codes.

--- extract.py
+++ extract.py
@@ -10,21 +10,18 @@ CONFIG_TEMP_PATH = Config.getValue('FILE', 'TEMP_PATH')
 CONFIG_TEMP_ZIP_FILE_NAME = Config.getValue('FILE', 'TEMP_ZIP_FILE_NAME')
 
 TEMP_PATH = os.path.join(CONFIG_TEMP_PATH, 'img')
-TEMP_ZIP_PATH = CONFIG_TEMP_PATH
 
 TIME = [(0, 4), (8, 12), (15, 20), (40, 50), (50, 60)]
 
-Comm.checkAndCreateDirectory(TEMP_ZIP_PATH)
+Comm.checkAndCreateDirectory(CONFIG_TEMP_PATH)
 Comm.checkAndCreateDirectory(TEMP_PATH)
 
-# print(Trans.downloadFileFromDropbox(TEMP_PATH, 'https://www.dropbox.com/s/wqb45wqt2lef9i4/toyota.mp4?dl=0'))
-
-video_path = Trans.downloadFile(os.path.join(TEMP_ZIP_PATH, 'temp.mp4')
+video_path = Trans.downloadFile(os.path.join(CONFIG_TEMP_PATH, 'temp.mp4')
                                 ,
                                 'http://0.s3.envato.com/h264-video-previews/80fad324-9db4-11e3-bf3d-0050569255a8/490527.mp4')
 
 video = VideoFileClip(filename=video_path, audio=False, verbose=True)
-zip = zipfile.ZipFile(os.path.join(TEMP_ZIP_PATH, CONFIG_TEMP_ZIP_FILE_NAME), 'w')
+zip = zipfile.ZipFile(os.path.join(CONFIG_TEMP_PATH, CONFIG_TEMP_ZIP_FILE_NAME), 'w')
 
 for i in TIME:
     for k in range(i[0], i[1] + 1):
@@ -36,4 +33,4 @@ for i in TIME:
 zip.close()
 print(zip.filename)
 Trans.uploadFileToBucket('whatsit-dataset-video', zip.filename, key='test.zip')
-print(shutil.rmtree(TEMP_ZIP_PATH))
+print(shutil.rmtree(CONFIG_TEMP_PATH))

34f9189c4079a4acc1b844e458160c653bd27437
bluehack-jhlim <jh.lim@bluehack.net>
2017-09-20 16:17:31 +0900

    [WHATSIT-115] Modified the put format.

--- util/Extract.py
+++ util/Extract.py
@@ -23,32 +23,39 @@ class Extract:
         source = data_set['source']
 
         sections = data_set['sections']
-
         video_path = Trans.download_file(os.path.join(self.__path, 'temp.mp4')
                                          , source)
 
         video = VideoFileClip(filename=video_path, audio=False, verbose=True)
         zip = zipfile.ZipFile(os.path.join(self.__path, 'temp.zip'), 'w')
-
+        image_files = []
         print('[Extracted image file from video]')
         for i in sections:
             for k in np.arange(i[0], i[1] + 1, 0.2):
-                filePath = os.path.join(self.__img_path, str(k) + '.jpg')
-                video.save_frame(filename=filePath, t=k)
-                zip.write(filePath, os.path.relpath(filePath, self.__img_path), compress_type=zipfile.ZIP_DEFLATED)
-                print(filePath)
+                image_inform = {'name': str(k) + '.jpg'}
+                image_files.append(image_inform)
+                image_file_path = os.path.join(self.__img_path, str(k) + '.jpg')
+                video.save_frame(filename=image_file_path, t=k)
+                zip.write(image_file_path, os.path.relpath(image_file_path, self.__img_path),
+                          compress_type=zipfile.ZIP_DEFLATED)
+                print(image_file_path)
 
         zip.close()
         print('\n\nCreated zip file::' + zip.filename)
         file_url = Trans.upload_file_to_bucket('whatsit-dataset-video', zip.filename,
                                                key=self.__dataset_id + '/' + video_name + '.zip', is_public=True)
-
+        print('image_files' + image_files.__str__())
         print(file_url)
         print('Deleted temp directory::')
         shutil.rmtree(self.__path)
         params = {
             "type": "video",
-            "data": [{"frames": file_url}]
+            "data": [{
+                "frames": file_url
+                , "images": image_files
+            }]
         }
+
+        print(params)
         print('Requested::')
         print(Trans.request_service('PUT', 'http://api.whatsit.net/datasets/' + self.__dataset_id, params))
